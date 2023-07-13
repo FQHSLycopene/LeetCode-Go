f0b013a2c3ca08dc342bed3a78599181e9b465a9
Lycopene <fqhslycopene@gmail.com>
2023-07-13 13:58:48 +0800

    005

--- leetcode/0005.Longest-Palindromic-Substring/5. Longest Palindromic Substring.go
+++ leetcode/0005.Longest-Palindromic-Substring/5. Longest Palindromic Substring.go
@@ -115,3 +115,39 @@ func longestPalindrome3(s string) string {
 	}
 	return res
 }
+
+func longestPalindrome4(s string) string {
+	n, m, i, j := 0, 0, 0, 0
+	for k := 0; ; k++ {
+		if j+1 == len(s) {
+			break
+		}
+		if k%2 == 0 {
+			i = k/2 + 1
+			j = k / 2
+		} else {
+			i, j = k/2+1, k/2+1
+		}
+		if s[i] != s[j] {
+			continue
+		}
+		for {
+			if i+1 == len(s) || j == 0 {
+				break
+			}
+			if s[i+1] == s[j-1] {
+				i++
+				j--
+				continue
+			} else {
+				break
+			}
+
+		}
+		if i-j > m-n {
+			m = i
+			n = j
+		}
+	}
+	return s[n : m+1]
+}
--- test/test.go
+++ test/test.go
@@ -3,56 +3,41 @@ package main
 import "fmt"
 
 func main() {
-	a := findMedianSortedArraysLycopene([]int{1}, []int{2, 3, 4, 5, 6, 7, 8})
-	fmt.Println(a)
+	fmt.Println(longestPalindrome4("aacabdkacaa"))
 }
 
-func findMedianSortedArraysLycopene(nums1 []int, nums2 []int) float64 {
-	l := len(nums1) + len(nums2)
-	if l == 0 {
-		return 0
-	}
-	if l == 1 {
-		return getKth(nums1, nums2, 0)
-	}
-	if (l)%2 == 0 {
-		return (getKth(nums1, nums2, l/2-1) + getKth(nums1, nums2, l/2)) / 2
-	} else {
-		return getKth(nums1, nums2, l/2)
-	}
-	return 0
-}
-
-func getKth(nums1 []int, nums2 []int, k int) float64 {
-	if len(nums1) == 0 {
-		return float64(nums2[k])
-	}
-	if len(nums2) == 0 {
-		return float64(nums1[k])
-	}
-
-	if k == 0 {
-		if nums1[0] > nums2[0] {
-			return float64(nums2[0])
+func longestPalindrome4(s string) string {
+	n, m, i, j := 0, 0, 0, 0
+	for k := 0; ; k++ {
+		if j+1 == len(s) {
+			break
 		}
-		return float64(nums1[0])
-	} else if k == 1 {
-		if nums1[0] < nums2[0] {
-			return getKth(nums1[1:], nums2, k-1)
+		if k%2 == 0 {
+			i = k/2 + 1
+			j = k / 2
+		} else {
+			i, j = k/2+1, k/2+1
 		}
-		return getKth(nums1, nums2[1:], k-1)
-	}
-	n1, n2 := k/2-1, k/2-1
-	if k/2-1 >= len(nums1) {
-		n1 = len(nums1) - 1
-	}
-	if k/2-1 >= len(nums2) {
-		n2 = len(nums2) - 1
-	}
-	if nums1[n1] > nums2[n2] {
-		return getKth(nums1, nums2[n2+1:], k-n2-1)
-	} else {
-		return getKth(nums1[n1+1:], nums2, k-n1-1)
-	}
+		if s[i] != s[j] {
+			continue
+		}
+		for {
+			if i+1 == len(s) || j == 0 {
+				break
+			}
+			if s[i+1] == s[j-1] {
+				i++
+				j--
+				continue
+			} else {
+				break
+			}
 
+		}
+		if i-j > m-n {
+			m = i
+			n = j
+		}
+	}
+	return s[n : m+1]
 }
